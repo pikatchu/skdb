7c035da830ff6e8f3a7b950bcd4ae960480d7b27
Julien Verlaguet <julien@skiplabs.io>
2025-05-11 16:38:49 +0300

    ++

--- skiplang/prelude/runtime/palloc.c
+++ skiplang/prelude/runtime/palloc.c
@@ -664,6 +664,11 @@ int sk_is_nofile_mode() {
 
 extern SKIP_gc_type_t* epointer_ty;
 
+void init_external_pointers() {
+  char* obj = sk_get_external_pointer();
+  epointer_ty = get_gc_type(obj);
+}
+
 void SKIP_memory_init(int argc, char** argv) {
   int is_create = 0;
   char* fileName = parse_args(argc, argv, &is_create);
@@ -690,8 +695,7 @@ void SKIP_memory_init(int argc, char** argv) {
   }
 #endif  // __APPLE__
 
-  char* obj = sk_get_external_pointer();
-  epointer_ty = get_gc_type(obj);
+  init_external_pointers();
 }
 
 /*****************************************************************************/
